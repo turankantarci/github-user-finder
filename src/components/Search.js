3433916e2f9aaf493614e4ec20d022edbb5f2858
Turan Kantarcı <hanedanturan@gmail.com>
2021-05-11 22:22:38 +0300

    useRef for search input value

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,40 +1,33 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import GithubContext from '../context/githubContext'
 import { useHistory } from 'react-router-dom'
 
 const Search = (props) => {
-    const [ keyword, setKeyword ] = useState('');
     const history = useHistory();
     const { searchUsers } = useContext(GithubContext);
-
-    const searchInputChange = (e) => {
-        setKeyword(e.target.value);
-    }
+    const searchInput = useRef();
 
     const searchUser = () => {
-        if (keyword) {
-            searchUsers(keyword);
-            setKeyword('');
-            history.push("/");
-        }
+        searchUsers(searchInput.current.value);
+        history.push("/");
     }
 
     return (
-        <React.Fragment>
+        <>
             {props.isMainSearch ? (
                 <div className="col-12 input-group input-group-lg mt-5">
-                    <input type="text" className="form-control" placeholder="Search any user..." onChange={searchInputChange} onKeyPress={e => e.key === "Enter" && searchUser()} />
+                    <input type="text" ref={searchInput} className="form-control" placeholder="Search any user..." onKeyPress={e => e.key === "Enter" && searchUser()} />
                     <div className="input-group-append">
                         <button className="btn btn-primary" type="button" onClick={searchUser}>Search</button>
                     </div>
                 </div>
             ) : (
                 <form className="form-inline" onSubmit={(e) => e.preventDefault()}>
-                    <input className="form-control mr-2" type="text" value={keyword} placeholder="Username" onChange={searchInputChange} onKeyPress={e => e.key === "Enter" && searchUser()} />
+                    <input type="text" ref={searchInput} className="form-control mr-2" placeholder="Username" onKeyPress={e => e.key === "Enter" && searchUser()} />
                     <button type="button" className="btn btn-outline-light" onClick={searchUser}>Search</button>
                 </form>
             )}
-        </React.Fragment>
+        </>
     )
 }
 
